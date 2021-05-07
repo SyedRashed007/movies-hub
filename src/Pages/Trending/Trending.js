86fdf43e4ab806dd9a8e9e7f48b7bad8881163ea
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-08 03:06:56 +0530

    Props

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import SingleContent from '../../components/SingleContent/SingleContent'
+import './Trending.css'
 
 function Trending() {
 
@@ -10,7 +12,7 @@ function Trending() {
     const fetchTrending = async () => {
         const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`)
     
-        console.log(data)
+        // console.log(data)
         setContent(data.results);
         
     }
@@ -25,9 +27,17 @@ function Trending() {
             <span className='pageTitle'>Trending</span>
             <div className='trending'>
                 {
-                    content && content.map((c)=> {
-                        console.log(c)
-                    })
+                    content && content.map((c)=> (
+                        <SingleContent 
+                            key={c.id}
+                            id={c.id}
+                            poster={c.poster_path}
+                            title={c.title || c.name}
+                            date={c.first_air_date || c.release_date}
+                            media_type={c.media_type}
+                            vote_average={c.vote_average}
+                        />
+                    ))
                 }
             </div>
         </div>
