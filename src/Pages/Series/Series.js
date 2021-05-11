e4dcf5f9aaca9fd412a47cfe89a1da10445394c9
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-12 02:42:44 +0530

    Fixed

--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Genres from '../../components/Genres/Genres'
+import CustomPagination from '../../components/Pagination/CustomPagination'
+import SingleContent from '../../components/SingleContent/SingleContent'
+import useGenres from '../../hooks/useGenres'
 
 function Series() {
+
+
+    const [ content, setContent ] = useState([])
+    const [ page, setPage ] = useState(1) 
+    const [numOfPages, setnumOfPages] = useState()
+    const [selectedGenres, setSelectedGenres] = useState([])
+    const [genres, setGenres] = useState([])
+    const genreforURL = useGenres(selectedGenres)
+
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const {data} = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`)
+            // console.log(data)
+            setContent(data.results);
+            setnumOfPages(data.total_pages)
+        }
+            fetchMovies();
+
+    }, [page, genreforURL])
+
+
+
+
     return (
-        <div className='pageTitle'>
-            Series
+        <div>
+            <span className='pageTitle'>TV Series</span>
+            <Genres
+                        type='tv' 
+                        selectedGenres={selectedGenres} 
+                        setSelectedGenres={setSelectedGenres} 
+                        genres={genres} 
+                        setGenres={setGenres}
+                        setPage={setPage}
+            />
+                    <div className='trending'>
+                            {
+                                content && content.map((c)=> (
+                                    <SingleContent 
+                                        key={c.id}
+                                        id={c.id}
+                                        poster={c.poster_path}
+                                        title={c.title || c.name}
+                                        date={c.first_air_date || c.release_date}
+                                        media_type='tv'
+                                        vote_average={c.vote_average}
+                                    />
+                                ))
+                            }
+                    </div>
+                            {
+                                numOfPages > 1 &&(
+                                    <CustomPagination setPage={setPage} numOfPages={numOfPages}/>
+                                )
+                            }
         </div>
     )
 }
