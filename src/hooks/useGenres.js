b34a0f64efe2ae8b9884f5c9b591e247bccc0782
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-12 02:41:37 +0530

    Added function

--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -1,7 +1,8 @@
-// import React from 'react'
+function useGenres(selectedGenres) {
+    if (selectedGenres.length < 1) return "";
 
-// function useGenres(selectedGenres) {
-//     if (selectedGenres.length < 1) return "";
-// }
+    const GenresIds = selectedGenres.map((g) => g.id);
+    return GenresIds.reduce((acc, curr)=> acc + "," + curr)
+}
 
-// export default useGenres
+export default useGenres
