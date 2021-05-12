d82685e5e0884d098b46fa2420f7677677da53cf
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-13 03:03:15 +0530

    Added function

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,12 +1,18 @@
 import { Button, createMuiTheme, Tab, Tabs, TextField, ThemeProvider } from "@material-ui/core"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import SearchIcon from '@material-ui/icons/Search'
+import axios from "axios"
+import CustomPagination from "../../components/Pagination/CustomPagination"
+import SingleContent from '../../components/SingleContent/SingleContent'
+
 
 function Search() {
 
     const [type, setType] = useState(0)
     const [page, setPage] = useState(1)
-    // const [searchText, setSearchText] = useState("")
+    const [searchText, setSearchText] = useState("")
+    const [content, setContent] = useState()
+    const [numOfPages, setNumOfPages] = useState()
 
     const darkTheme = createMuiTheme({
         palette: {
@@ -17,33 +23,80 @@ function Search() {
         }
     })
 
+
+    const fetchSearch = async () => {
+        try{
+            const { data } = await axios.get(`https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`)
+            console.log(data)
+            setContent(data.results);
+            setNumOfPages(data.total_pages);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    useEffect(() => {
+        window.scroll(0,0);
+        fetchSearch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [type, page])
+    // type and page when changed the function is called
+
+
+
+
     return (
         <div>
-        <ThemeProvider theme ={darkTheme}>
-            <div style={{display: "flex", margin: "15px 0"}}>
-                <TextField
-                    style={{ flex: 1 }}
-                    className='searchBox'
-                    label="Search"
-                    variant="filled"
-                />
-                <Button variant='contained' style={{marginLeft:10}}> <SearchIcon/>  </Button>
+            <ThemeProvider theme ={darkTheme}>
+                <div style={{display: "flex", margin: "15px 0"}}>
+                    <TextField
+                        style={{ flex: 1 }}
+                        className='searchBox'
+                        label="Search"
+                        variant="filled"
+                        onChange={(e) => setSearchText(e.target.value)}
+                        // setSearchtext here is used for searching here and hence api is called here
+                    />
+                    <Button 
+                        variant='contained' 
+                        style={{marginLeft:10}}
+                        onClick={fetchSearch}
+                        > <SearchIcon/>  </Button>
+                </div>
+
+                <Tabs 
+                    value={type} 
+                    indicatorColor='primary' 
+                    textColor='primary'
+                    onChange={(event, newValue) => {
+                        setType(newValue);
+                        setPage(1)
+                    }}
+                >
+                    <Tab style={{ width: '50%'}} label="Seach Movies"/>
+                    <Tab style={{ width: '50%'}} label="Seach Tv Series"/>
+                </Tabs>
+            </ThemeProvider>
+            <div className="trending">
+                {content &&
+                    content.map((c) => (
+                        <SingleContent
+                        key={c.id}
+                        id={c.id}
+                        poster={c.poster_path}
+                        title={c.title || c.name}
+                        date={c.first_air_date || c.release_date}
+                        media_type={type ? "tv" : "movie"}
+                        vote_average={c.vote_average}
+                        />
+                ))}
+                {searchText &&
+                !content &&
+                (type ? <h2>No Series Found</h2> : <h2>No Movies Found</h2>)}
             </div>
-
-            <Tabs 
-                value={type} 
-                indicatorColor='primary' 
-                textColor='primary'
-                onChange={(event, newValue) => {
-                    setType(newValue);
-                    setPage(1)
-                }}
-            >
-                <Tab style={{ width: '50%'}} label="Seach Movies"/>
-                <Tab style={{ width: '50%'}} label="Seach Tv Series"/>
-            </Tabs>
-
-        </ThemeProvider>
+            {numOfPages > 1 && (
+                <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+            )}
         </div>
     )
 }
